30792bde83aba911e5f8b76e3c10f9475e1f1e97
santuseera123 <santu.seera123@gmail.com>
2017-01-28 06:20:11 +0530

    it shows how to recording audio

---
+++ AudiorecordingApp/app/src/main/java/myapplicationishello/com/example/hsport/audiorecordingapp/MainActivity.java
@@ -0,0 +1,35 @@
+package myapplicationishello.com.example.hsport.audiorecordingapp;
+
+import android.media.MediaRecorder;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    MediaRecorder recorder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recorder = new MediaRecorder();
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.AMR_NB);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        recorder.setOutputFile("/storage/sdcard0/obb.amr");
+    }
+
+    public void start(View v) {
+        try {
+            recorder.prepare();
+            recorder.start();
+        } catch (Exception e) {
+        }
+    }
+
+    public void stop(View v) {
+
+        recorder.stop();
+    }
+}
