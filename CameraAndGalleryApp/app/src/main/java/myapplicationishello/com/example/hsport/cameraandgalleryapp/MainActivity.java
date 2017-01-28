0ded23800756a85f9eb7a0605622568307d8fede
santuseera123 <santu.seera123@gmail.com>
2017-01-28 06:24:06 +0530

    To pick the Image from Camera or Gallery

---
+++ CameraAndGalleryApp/app/src/main/java/myapplicationishello/com/example/hsport/cameraandgalleryapp/MainActivity.java
@@ -0,0 +1,49 @@
+package myapplicationishello.com.example.hsport.cameraandgalleryapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    String path=("/storage/sdcard0/obb.jpg");
+    File f;
+    public  void camera(View v){
+        Intent i=new Intent("android.media.action.IMAGE_CAPTURE");
+        f=new File(path);
+        i.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+        startActivityForResult(i,1);
+    }
+
+    public  void gallery(View v){
+        Intent i=new Intent();
+        i.setAction(Intent.ACTION_PICK);
+        i.setType("image/*");
+        startActivityForResult(i,2);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ImageView iView=(ImageView)findViewById(R.id.imageView);
+        if(requestCode==1){
+            iView.setImageURI(Uri.fromFile(f));
+        }
+        else if(requestCode==2){
+            iView.setImageURI(data.getData());
+        }
+    }
+}
+
+
