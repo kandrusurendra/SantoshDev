92041620de87b0018ed53296cd42e3ea5fce4408
santuseera123 <santu.seera123@gmail.com>
2017-01-28 06:22:21 +0530

    Add files via upload
    bluetooth Application,To Check Whether BlueTooth device Connected Or not

---
+++ BluetoothApp/app/src/main/java/myapplicationishello/com/example/hsport/bluetoothapp/MainActivity.java
@@ -0,0 +1,31 @@
+package myapplicationishello.com.example.hsport.bluetoothapp;
+
+import android.bluetooth.BluetoothAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+public class MainActivity extends AppCompatActivity {
+    Switch sw1;
+    BluetoothAdapter bAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sw1 = (Switch) findViewById(R.id.switch1);
+        bAdapter = BluetoothAdapter.getDefaultAdapter();
+        sw1.setChecked(bAdapter.isEnabled());
+        sw1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b) {
+                    bAdapter.enable();
+                } else {
+                    bAdapter.disable();
+                }
+            }
+        });
+    }
+
+}
