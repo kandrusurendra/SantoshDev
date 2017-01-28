a48693fd8887b19e9305e42485c50095b076b04b
santuseera123 <santu.seera123@gmail.com>
2017-01-28 06:26:59 +0530

    it is used to check wi-fi connected or not i

---
+++ WifiApp/app/src/main/java/myapplicationishello/com/example/hsport/wifiapp/MainActivity.java
@@ -0,0 +1,59 @@
+package myapplicationishello.com.example.hsport.wifiapp;
+
+import android.content.Context;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    WifiManager wmanager;
+    Switch sw1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        wmanager=(WifiManager)getSystemService(Context.WIFI_SERVICE);
+        sw1=(Switch)findViewById(R.id.switch1);
+        int i=wmanager.getWifiState();
+
+        if(i==0){
+            sw1.setChecked(false);
+        }else  if(i==3){
+            sw1.setChecked(true);
+        }
+
+        sw1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    wmanager.setWifiEnabled(true);
+                }else{
+                    wmanager.setWifiEnabled(false);
+                }
+            }
+        });
+    }
+    public void getscanresults(View v){
+        List<ScanResult>results=wmanager.getScanResults();
+        for(int i=0;i<results.size();i++){
+
+            ScanResult result=results.get(i);
+            Toast.makeText(getApplicationContext(),result.SSID+"/n"+result.frequency,2000).show();
+        }
+    }
+    public void  getconfigurednetworks(View v){
+        List<WifiConfiguration> configs=wmanager.getConfiguredNetworks();
+        for(int i=0;i<configs.size();i++){
+            WifiConfiguration result=configs.get(i);
+            Toast.makeText(getApplicationContext(),result.SSID+"/n"+result.status,2000).show();
+        }
+    }
+}
