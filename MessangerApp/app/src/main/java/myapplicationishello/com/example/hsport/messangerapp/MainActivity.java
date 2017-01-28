522ff37c441a026199730822d4dcdb805206e55f
santuseera123 <santu.seera123@gmail.com>
2017-01-28 06:25:42 +0530

    Chatting Application,it is used to Chat with other

---
+++ MessangerApp/app/src/main/java/myapplicationishello/com/example/hsport/messangerapp/MainActivity.java
@@ -0,0 +1,21 @@
+package myapplicationishello.com.example.hsport.messangerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+        public void button(View v) {
+        EditText et1 = (EditText) findViewById(R.id.et1);
+        TextView tv1 = (TextView) findViewById(R.id.tv1);
+        tv1.setText(tv1.getText()+et1.getText().toString()+"\n");
+    }
+}
